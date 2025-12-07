ff073d598fe1ce9a8caccf8538ebeb1d058e98e2
Alejandro Flores <dev.alejandro.flores@gmail.com>
2025-12-07 16:29:57 -0700

    feat: Esquema de base de datos, entity framework y repositorio de proveedores

--- MtoMigracion/Program.cs
+++ MtoMigracion/Program.cs
@@ -1,17 +1,25 @@
-namespace MtoMigracion
+using Microsoft.EntityFrameworkCore;
+
+using MtoMigracion.Core;
+using MtoMigracion.Interfaz;
+
+namespace MtoMigracion;
+
+internal static class Program
 {
-    internal static class Program
+    /// <summary>
+    ///  The main entry point for the application.
+    /// </summary>
+    [STAThread]
+    static void Main()
     {
-        /// <summary>
-        ///  The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
-        {
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
-            ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
-        }
+        var ctx = AppDbContextFactory.GetDbContext();
+        ctx.Database.Migrate();
+        ctx.SaveChanges();
+
+        // To customize application configuration such as set high DPI settings or default font,
+        // see https://aka.ms/applicationconfiguration.
+        ApplicationConfiguration.Initialize();
+        Application.Run(new MenuInicio());
     }
 }
