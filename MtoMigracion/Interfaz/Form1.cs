58bf5a1227b85c71008dd179527844400af5e267
Hiram <112568802+HiramNM@users.noreply.github.com>
2025-12-08 21:56:49 -0700

    Formulario1 (El del inicio)
    Interfaz grafica del formulario del menu inicial FInicio

---
+++ MtoMigracion/Interfaz/Form1.cs
@@ -0,0 +1,45 @@
+namespace Formulario1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox2_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
